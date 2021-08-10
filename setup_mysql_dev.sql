5dc58ea75282eaa52698fd12cd915d5ebe7365c6
01Eddie <jesus.11.97@hotmail.com>
2021-08-09 19:20:31 -0500

    [ADD] prepare projects

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,8 @@
+/* Write a script that prepares a MySQL server for the project:
+
+A database hbnb_dev_db
+A new user hbnb_dev (in localhost)
+The password of hbnb_dev should be set to hbnb_dev_pwd
+hbnb_dev should have all privileges on the database hbnb_dev_db (and only this database)
+hbnb_dev should have SELECT privilege on the database performance_schema (and only this database)
+If the database hbnb_dev_db or the user hbnb_dev already exists, your script should not fail */
