d3b6f8ac525fee0343e2e13a27299f083c8c4c7b
joseheco <jferhc@gmail.com>
2021-08-11 10:58:37 -0500

    state

--- models/state.py
+++ models/state.py
@@ -2,8 +2,15 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+import models
+from models.city import City
+
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
+    cities = relationship('City', cascade = 'all, delete', backref = 'states')
+
+    # falta return the list of city
