7ddd9340d52c13f74b6745691c8facb6c1beaa96
joseheco <jferhc@gmail.com>
2021-08-11 10:46:15 -0500

    punto_8_9_10

--- models/place.py
+++ models/place.py
@@ -2,6 +2,10 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy.orm import relationship
+import models
+from models.review import Review
+
 
 class Place(BaseModel):
     """ A place to stay """
@@ -17,3 +21,16 @@ class Place(BaseModel):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     amenity_ids = []
+    review = relationship('Review', cascade= 'all, delete', backref = 'place')
+    review = relationship('Amenity')   #punto 10 por acabar con setter y getter
+
+place_amenity = Table('place_amenity', Base.metadata,
+                        Column('place_id', ForeignKey('places.id'),
+                            nullable = False, String(60),
+                            primary_key = True)
+                        Column('amenity_id', String(60), 
+                            ForeignKey('amenities.id'), nullable = False,
+                            primary_key = True)
+                    )
+
+
