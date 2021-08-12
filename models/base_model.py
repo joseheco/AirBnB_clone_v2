fdc6e868fef256c83146508b796f7c6db7f4461a
joseheco <jferhc@gmail.com>
2021-08-12 18:54:48 -0500

    fixing

--- models/base_model.py
+++ models/base_model.py
@@ -26,7 +26,16 @@ class BaseModel:
     """Base for hbnb models"""
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        if self.id is None:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+        """if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
@@ -38,7 +47,7 @@ class BaseModel:
                     setattr(self, key, datetime.strptime(value, time))
                 else:
                     if key != '__class__':
-                        setattr(self, key, value)
+                        setattr(self, key, value)"""
 
     def __str__(self):
         """Returns a string representation of the instance"""
