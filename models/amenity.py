90e31122602f0a1d3bf0f09666565ab197836ff1
01Eddie <jesus.11.97@hotmail.com>
2021-08-12 17:09:27 -0500

    [FIX] amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
