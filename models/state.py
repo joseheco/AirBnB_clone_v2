187e225807741574c9fe451bb09a5956c301617a
joseheco <jferhc@gmail.com>
2021-08-11 21:59:35 -0500

    last_update

--- models/state.py
+++ models/state.py
@@ -11,7 +11,7 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship('City', cascade='all, delete', backref='states')
+    cities = relationship('City', cascade='all, delete', backref='state')
 
     if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
