e0c6bc21dd7a92c987ec15afb206bb6d9b67b5d3
joseheco <jferhc@gmail.com>
2021-08-11 21:52:11 -0500

    last_update

--- models/state.py
+++ models/state.py
@@ -3,9 +3,9 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-import models
 from models.city import City
-
+import models
+from os import getenv
 
 class State(BaseModel, Base):
     """ State class """
@@ -13,4 +13,9 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship('City', cascade='all, delete', backref='states')
 
-    # falta return the list of city
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """return the list of city"""
+            return [value for value in models.storage.all(City).value()
+                    if value.state_id == self.id]
