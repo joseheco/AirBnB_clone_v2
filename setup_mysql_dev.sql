5be76e5477938098808d46389f4e99db2689eab8
joseheco <jferhc@gmail.com>
2021-08-12 22:01:23 -0500

    fixing

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,11 +2,11 @@
 -- A database hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- A new user hbnb_dev (in localhost)
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';;
 -- The password of hbnb_dev should be set to hbnb_dev_pwd
 SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
 -- hbnb_dev should have all privileges on the database hbnb_dev_db (and only this database)
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@%; -- %:
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost'; -- %:
 -- hbnb_dev should have SELECT privilege on the database performance_schema (and only this database)
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 -- If the database hbnb_dev_db or the user hbnb_dev already exists, your script should not fail
