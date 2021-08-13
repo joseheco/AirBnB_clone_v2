c1f1f0ebadd5430ea9032942ab685a168e7ba5b0
joseheco <jferhc@gmail.com>
2021-08-12 20:00:38 -0500

    fixing

--- models/base_model.py
+++ models/base_model.py
@@ -30,6 +30,28 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
+            tm = '%Y-%m-%dT%H:%M:%S.%f'
+
+            if 'id' not in kwargs:
+                kwargs['id'] = str(uuid.uuid4())
+            if 'updated_at' not in kwargs:
+                kwargs['updated_at'] = datetime.now()
+            else:
+                kwargs['updated_at'] = datetime\
+                                        .strptime(kwargs['updated_at'], tm)
+            if 'created_at' not in kwargs:
+                kwargs['created_at'] = datetime.now()
+            else:
+                kwargs['created_at'] = datetime\
+                                        .strptime(kwargs['created_at'], tm)
+            if '__class__' in kwargs:
+                del kwargs['__class__']
+            self.__dict__.update(kwargs)
+        """if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
                         # value: %y, %m, %d
@@ -37,7 +59,7 @@ class BaseModel:
                     setattr(self, key, datetime.strptime(value, time))
                 else:
                     if key != '__class__':
-                        setattr(self, key, value)
+                        setattr(self, key, value)"""
 
     def __str__(self):
         """Returns a string representation of the instance"""
