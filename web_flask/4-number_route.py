2b4f25f685a95141d6199862b104bc8d74ce0bc7
joseheco <jferhc@gmail.com>
2021-09-12 18:18:01 -0500

    Flask

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Start flask application"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """Return hello hbnb"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Return hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """Returns a string at the c"""
+    return "C {:s}".format(text.replace("_", " "))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text="is cool"):
+    """display messagei is cool"""
+    return "Python {:s}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """display a number if is int"""
+    return "{:d} is a number".format(n)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
