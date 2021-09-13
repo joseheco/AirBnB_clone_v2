2ce94798e82cc1f3862b1644c602d837aec07425
joseheco <jferhc@gmail.com>
2021-09-12 19:19:15 -0500

    Flask

--- models/state.py
+++ models/state.py
@@ -13,7 +13,7 @@ class State(BaseModel, Base):
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
-        cities = relationship('City', cascade='delete', backref='state')
+        cities = relationship('City', cascade='all, delete-orphan', backref='state')
     else:
         name = ""
 
